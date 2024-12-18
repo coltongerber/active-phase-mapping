7948e2d6091d84fccc315f9dde3bcf39b2cb618b
coltongerber <coltonjgerber@gmail.com>
2024-12-18 12:05:48 -0700

    feature: Add option to resume from previous run

--- active_learning/cal.py
+++ active_learning/cal.py
@@ -33,6 +33,7 @@ parser.add_argument('-directory', type=str, help='path to results directory')
 parser.add_argument('-truth_df', type=str, help="path to DataFrame JSON containing true y-data")
 parser.add_argument('-method', type=str, help='Type of Baseline search method')
 parser.add_argument('-seed_range', type=int, nargs="+", help='Seed Range')
+parser.add_argument('-resume', type=bool, help='Resume from previous run')
 args = parser.parse_args()
 
 system(f'mkdir {args.directory}')
@@ -55,148 +56,177 @@ for seed in range(args.seed_range[0],args.seed_range[1]):
     method=args.method
     iterations = args.num_iter
     truth_df = args.truth_df
+    resume = args.resume
+    if resume:
+        master_hull_dict = pd.read_pickle(f'{args.directory}/hull.pkl')
+        master_energy_dict = pd.read_pickle(f'{args.directory}/energy.pkl')
+        master_dataset_dict = pd.read_pickle(f'{args.directory}/observation.pkl')
+        master_problem_setup_dict = pd.read_pickle(f'{args.directory}/problem_setup.pkl')
+        poly_dict = master_dataset_dict[seed][-1]
+        pts = poly_dict[0]['true_y_dict'].keys()
+        design_space = np.array(pts)[:,:dimensions-1]
+        knot_N = len(design_space)
+        endpoint_indices = get_endpoint_indices(dimensions,pts)
+        true_classifications = master_problem_setup_dict[seed]['True Hull Classifications']
+        hull_dict={}
+        energy_dict={}
+        dataset_dict={}
+        df = pd.read_csv(f'{args.directory}/performance_seed_{seed}.csv')
 
-    poly_dict={}
-    for i in range(num_polymorphs):
-        poly_dict[i]=deepcopy({'true_y':[],'design_space':[],'pred_mean':[],
-        'pred_cov':[],'posterior':[],'params':[],'train_x':[],'train_y':[],
-        'dataset':[]})
-
-    #Producing grid
-    if truth_df:
-        # For using G as true_y
-        prism_df = pd.read_json(truth_df, orient="records", lines=True)
-        # prism_df = prism_df[(prism_df["S"] != 2.0) & (prism_df["Se"] != 2.0) & (prism_df["Te"] != 2.0) & (prism_df["Pb"] != 2.0) & (prism_df["Sn"] != 2.0)]
-        prism_df["reduced_point"] = prism_df.apply(lambda x: [x["S"]/2, x["Se"]/2, x["Te"]/2, x["Pb"]/2, x["Sn"]/2], axis=1)
-        pts = np.array(prism_df["reduced_point"].to_list())
-    else:
-        pts=nD_coordinates(dimensions,0,1,n_grid)
-
-    design_space=np.array(pts)[:,:dimensions-1]
-
-    endpoint_indices = get_endpoint_indices(dimensions,pts)
-    #removing endpoint indices from list of candidates.
-    designs = [x for index,x in enumerate(design_space) if index not in endpoint_indices]
-    knot_N = len(design_space)
-    print(knot_N)
-    alpha=[0,0.25,0.5]
-    for i in range(num_polymorphs):
-        #Generating energy  surfaces
-        if truth_df:
-            y = prism_df["G_leveled_meV_atom"].to_numpy()
-            lin_comb=get_lin_comb(pts,endpoint_indices, y)
-            poly_dict[i]['true_y'] = y - lin_comb
-        else:
-            poly_dict[i]['true_y'] = generate_true_function(design_space, knot_N) + alpha[i]*i
-
-        #poly_dict[i]['std'], poly_dict[i]['mean'] = poly_dict[i]['true_y'].std(), poly_dict[i]['true_y'].mean()
-        #plt.plot(np.ravel(design_space),poly_dict[i]['true_y'])
-        #plt.show()
-        poly_dict[i]['true_y_dict'] = {}
-        for comp, y in zip(design_space, poly_dict[i]['true_y']):
-            comp=tuple([float(i) for i in comp])
-            poly_dict[i]['true_y_dict'][comp]=y
-
-        #Training on endpoint indices.
-        x_lst,y_lst=[],[]
-        for endpoint_index in endpoint_indices:
-            x_lst.append(design_space[endpoint_index])
-            y_lst.append(poly_dict[i]['true_y'][endpoint_index])
-
-        # For using G as true_y with pure compounds as simplex endpoints.
-        # This will add the endpoints of the pseudobinary space of interest to 
-        # the initial set of points.
-        # if truth_df:
-        #     pts_list = pts.tolist()
-        #     mask = np.array([x.count(0.5) for x in pts_list]) == 2
-        #     design_space_endpoints = np.array(pts_list)[mask].tolist()
-        #     design_space_endpoint_indices = []
-        #     for endpoint in design_space_endpoints:
-        #         design_space_endpoint_indices.append(pts_list.index(endpoint))
-        #     for endpoint_index in design_space_endpoint_indices:
-        #         x_lst.append(design_space[endpoint_index])
-        #         y_lst.append(poly_dict[i]['true_y'][endpoint_index])
-                
-        y_array=jnp.array(y_lst)[:,jnp.newaxis]
-        y_mean=jnp.array(y_lst)[:,jnp.newaxis].mean()
-        y_std=jnp.array(y_lst)[:,jnp.newaxis].std()
-        if y_std == 0:
-            normalized_y =  (y_array-y_mean)
-        else:
-            normalized_y =  (y_array-y_mean) / y_std 
-        poly_dict[i]['std'] = deepcopy(y_std)
-        poly_dict[i]['mean'] = deepcopy(y_mean)
-        poly_dict[i]['dataset'] = deepcopy(Dataset(X=jnp.array(x_lst), y=normalized_y))
-        poly_dict[i]['designs'] = deepcopy(designs)
-        poly_dict[i]['pred_mean'], poly_dict[i]['pred_cov'], poly_dict[i]['posterior'], poly_dict[i]['params'] = update_model(
-        poly_dict[i]['dataset'], design_space, rng_key, update_params=False) # Update the model given the data above
-    index_dict={}
-    for index,x in enumerate(design_space):
-        comp_tuple=tuple([float(i) for i in x])
-        index_dict[comp_tuple]=index
-    ###Determining true_hull
-    #Producing min_curve
-    all_true_curves=[]
-    for i in range(num_polymorphs):
-        all_true_curves.append(poly_dict[i]['true_y'])
-    min_curve=np.min(np.vstack(all_true_curves),axis=0)
-    #print(min_curve)
-    #Set endpoints to zero.
-    lin_comb=get_lin_comb(pts,endpoint_indices,min_curve)
-    Y_zeroed=min_curve-lin_comb
-    
-    # If nothing is negative, there will only be two points passed to QHull,
-    # and it will fail. Do not use QHull in this case.
-    if (np.delete(Y_zeroed, endpoint_indices) < 0).sum() == 0:
-        true_e_hull = lin_comb
-    else:
-        #Determining vertices of hull
-        true_e_hull=lin_comb + get_hull_energies(design_space,Y_zeroed,endpoint_indices=endpoint_indices, dimensions=dimensions)
+        energy_error_means = df['energy_error_means'].to_list()
+        energy_error_stds = df['energy_error_stds'].to_list()
+        initial_entropies = df['Entropy'].to_list()
+        true_positive_rates = df['true_positive_rate'].to_list()
+        false_positive_rates = df['false_positive_rate'].to_list()
+        iteration_times = df['time (s)'].to_list()
+        its = df['iteration'].to_list()
+        dfs.append(df)
         
-    tol=1e-3
-    vertices=(min_curve-true_e_hull)<tol
-    true_classifications=jnp.zeros(knot_N).at[vertices].set(1)
-    print('ON HULL',np.sum(true_classifications))
-    ###Active search
-    energies={}
-    scale=n_grid-1
-    its=[]
-    energy_error_means=[]
-    energy_error_stds=[]
-    false_positive_rates=[]
-    true_positive_rates=[]
-    initial_entropies=[]
-    iteration_times=[]
-    hull_dict={}
-    energy_dict={}
-    dataset_dict={}
-    num_curves=args.num_curves
-    num_samples=args.num_samples
-    num_y = args.num_y
+        last_it = its[-1]
+        iter_range = range(last_it + 1, iterations)
+        
+    else: # start new run
+        poly_dict={}
+        for i in range(num_polymorphs):
+            poly_dict[i]=deepcopy({'true_y':[],'design_space':[],'pred_mean':[],
+            'pred_cov':[],'posterior':[],'params':[],'train_x':[],'train_y':[],
+            'dataset':[]})
 
-    problem_setup_dict={}
-    problem_setup_dict['Grid Spacing']=n_grid
-    problem_setup_dict['Number of Samples']=num_samples
-    problem_setup_dict['Method']=method
-    problem_setup_dict['Dimensions (Including Implicit']=dimensions
-    problem_setup_dict['offset']=alpha
-    problem_setup_dict['Number of Polymorphs']=1
-    problem_setup_dict['Points']=pts
-    problem_setup_dict['Design Space']=design_space
-    problem_setup_dict['Lengthscale']=0.2
-    problem_setup_dict['Kernel']='RBF'
-    problem_setup_dict['Noise']=0
-    problem_setup_dict['Variance']=1
-    problem_setup_dict['Poly Dict']=poly_dict
-    problem_setup_dict['True E_Hull']=true_e_hull
-    problem_setup_dict['True Hull Vertices']=vertices
-    problem_setup_dict['True Hull Classifications']=true_classifications
-    problem_setup_dict['Energy Classification Tolerance']=tol
-    problem_setup_dict['Index Dict']=index_dict
-    master_problem_setup_dict[seed]=problem_setup_dict
+        #Producing grid
+        if truth_df:
+            # For using G as true_y
+            prism_df = pd.read_json(truth_df, orient="records", lines=True)
+            # prism_df = prism_df[(prism_df["S"] != 2.0) & (prism_df["Se"] != 2.0) & (prism_df["Te"] != 2.0) & (prism_df["Pb"] != 2.0) & (prism_df["Sn"] != 2.0)]
+            prism_df["reduced_point"] = prism_df.apply(lambda x: [x["S"]/2, x["Se"]/2, x["Te"]/2, x["Pb"]/2, x["Sn"]/2], axis=1)
+            pts = np.array(prism_df["reduced_point"].to_list())
+        else:
+            pts=nD_coordinates(dimensions,0,1,n_grid)
 
+        design_space=np.array(pts)[:,:dimensions-1]
 
-    for it in tqdm(range(iterations)):
+        endpoint_indices = get_endpoint_indices(dimensions,pts)
+        #removing endpoint indices from list of candidates.
+        designs = [x for index,x in enumerate(design_space) if index not in endpoint_indices]
+        knot_N = len(design_space)
+        print(knot_N)
+        alpha=[0,0.25,0.5]
+        for i in range(num_polymorphs):
+            #Generating energy  surfaces
+            if truth_df:
+                y = prism_df["G_leveled_meV_atom"].to_numpy()
+                lin_comb=get_lin_comb(pts,endpoint_indices, y)
+                poly_dict[i]['true_y'] = y - lin_comb
+            else:
+                poly_dict[i]['true_y'] = generate_true_function(design_space, knot_N) + alpha[i]*i
+
+            #poly_dict[i]['std'], poly_dict[i]['mean'] = poly_dict[i]['true_y'].std(), poly_dict[i]['true_y'].mean()
+            #plt.plot(np.ravel(design_space),poly_dict[i]['true_y'])
+            #plt.show()
+            poly_dict[i]['true_y_dict'] = {}
+            for comp, y in zip(design_space, poly_dict[i]['true_y']):
+                comp=tuple([float(i) for i in comp])
+                poly_dict[i]['true_y_dict'][comp]=y
+
+            #Training on endpoint indices.
+            x_lst,y_lst=[],[]
+            for endpoint_index in endpoint_indices:
+                x_lst.append(design_space[endpoint_index])
+                y_lst.append(poly_dict[i]['true_y'][endpoint_index])
+
+            # For using G as true_y with pure compounds as simplex endpoints.
+            # This will add the endpoints of the pseudobinary space of interest to 
+            # the initial set of points.
+            # if truth_df:
+            #     pts_list = pts.tolist()
+            #     mask = np.array([x.count(0.5) for x in pts_list]) == 2
+            #     design_space_endpoints = np.array(pts_list)[mask].tolist()
+            #     design_space_endpoint_indices = []
+            #     for endpoint in design_space_endpoints:
+            #         design_space_endpoint_indices.append(pts_list.index(endpoint))
+            #     for endpoint_index in design_space_endpoint_indices:
+            #         x_lst.append(design_space[endpoint_index])
+            #         y_lst.append(poly_dict[i]['true_y'][endpoint_index])
+                    
+            y_array=jnp.array(y_lst)[:,jnp.newaxis]
+            y_mean=jnp.array(y_lst)[:,jnp.newaxis].mean()
+            y_std=jnp.array(y_lst)[:,jnp.newaxis].std()
+            if y_std == 0:
+                normalized_y =  (y_array-y_mean)
+            else:
+                normalized_y =  (y_array-y_mean) / y_std 
+            poly_dict[i]['std'] = deepcopy(y_std)
+            poly_dict[i]['mean'] = deepcopy(y_mean)
+            poly_dict[i]['dataset'] = deepcopy(Dataset(X=jnp.array(x_lst), y=normalized_y))
+            poly_dict[i]['designs'] = deepcopy(designs)
+            poly_dict[i]['pred_mean'], poly_dict[i]['pred_cov'], poly_dict[i]['posterior'], poly_dict[i]['params'] = update_model(
+            poly_dict[i]['dataset'], design_space, rng_key, update_params=False) # Update the model given the data above
+        index_dict={}
+        for index,x in enumerate(design_space):
+            comp_tuple=tuple([float(i) for i in x])
+            index_dict[comp_tuple]=index
+        ###Determining true_hull
+        #Producing min_curve
+        all_true_curves=[]
+        for i in range(num_polymorphs):
+            all_true_curves.append(poly_dict[i]['true_y'])
+        min_curve=np.min(np.vstack(all_true_curves),axis=0)
+        #print(min_curve)
+        #Set endpoints to zero.
+        lin_comb=get_lin_comb(pts,endpoint_indices,min_curve)
+        Y_zeroed=min_curve-lin_comb
+        
+        # If nothing is negative, there will only be two points passed to QHull,
+        # and it will fail. Do not use QHull in this case.
+        if (np.delete(Y_zeroed, endpoint_indices) < 0).sum() == 0:
+            true_e_hull = lin_comb
+        else:
+            #Determining vertices of hull
+            true_e_hull=lin_comb + get_hull_energies(design_space,Y_zeroed,endpoint_indices=endpoint_indices, dimensions=dimensions)
+            
+        tol=1e-3
+        vertices=(min_curve-true_e_hull)<tol
+        true_classifications=jnp.zeros(knot_N).at[vertices].set(1)
+        print('ON HULL',np.sum(true_classifications))
+        ###Active search
+        energies={}
+        scale=n_grid-1
+        its=[]
+        energy_error_means=[]
+        energy_error_stds=[]
+        false_positive_rates=[]
+        true_positive_rates=[]
+        initial_entropies=[]
+        iteration_times=[]
+        hull_dict={}
+        energy_dict={}
+        dataset_dict={}
+        num_curves=args.num_curves
+        num_samples=args.num_samples
+        num_y = args.num_y
+
+        problem_setup_dict={}
+        problem_setup_dict['Grid Spacing']=n_grid
+        problem_setup_dict['Number of Samples']=num_samples
+        problem_setup_dict['Method']=method
+        problem_setup_dict['Dimensions (Including Implicit']=dimensions
+        problem_setup_dict['offset']=alpha
+        problem_setup_dict['Number of Polymorphs']=1
+        problem_setup_dict['Points']=pts
+        problem_setup_dict['Design Space']=design_space
+        problem_setup_dict['Lengthscale']=0.2
+        problem_setup_dict['Kernel']='RBF'
+        problem_setup_dict['Noise']=0
+        problem_setup_dict['Variance']=1
+        problem_setup_dict['Poly Dict']=poly_dict
+        problem_setup_dict['True E_Hull']=true_e_hull
+        problem_setup_dict['True Hull Vertices']=vertices
+        problem_setup_dict['True Hull Classifications']=true_classifications
+        problem_setup_dict['Energy Classification Tolerance']=tol
+        problem_setup_dict['Index Dict']=index_dict
+        master_problem_setup_dict[seed]=problem_setup_dict
+        iter_range = range(iterations)
+
+    for it in tqdm(iter_range):
         print("Iteration: ", it)
         initial_time=time()
 
@@ -205,7 +235,7 @@ for seed in range(args.seed_range[0],args.seed_range[1]):
         hull_expected_energy, false_positive_rate, true_positive_rate, initial_entropy, hull_samples, energy_samples, all_poly_samples  =calc_expected_energy_or_entropy(
         design_space=design_space, num_curves=num_curves, num_samples=num_samples, knot_N=knot_N, pts=pts, endpoint_indices=endpoint_indices,
         dimensions=dimensions, true_hull_classifications=true_classifications, poly_dict=poly_dict, multi=True, entropy_type=entropy_type)
-
+        
         hull_dict[it]=hull_samples
         energy_dict[it]=all_poly_samples
         dataset_dict[it]=deepcopy(poly_dict)
@@ -288,6 +318,7 @@ for seed in range(args.seed_range[0],args.seed_range[1]):
         save_pickle(path=f'{args.directory}/energy.pkl',item=master_energy_dict)
         save_pickle(path=f'{args.directory}/observation.pkl',item=master_dataset_dict)
         save_pickle(path=f'{args.directory}/problem_setup.pkl',item=master_problem_setup_dict)
+        
 
     master_df=pd.concat(dfs)
     master_df.to_csv(f'{args.directory}/performance.csv')
